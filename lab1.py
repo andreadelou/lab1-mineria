1134d1a5edf2bf400ae76acbe2b0045f79efedb8
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2023-02-02 19:42:14 -0600

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-datos = pd.read_csv('C:\Users\andre\Downloads\datos.csv')
-X = datos.iloc[:, :-1].values
-y = datos.iloc[:, -1].values
-
+datos = pd.read_csv(r"C:\Users\andre\Downloads\datos.csv")
+# print(datos.to_string()) 
 datos.head()
+# X = datos.iloc[:, :-1].values
+# y = datos.iloc[:, -1].values
+# datos.head()
+# print(X)
 
-print(X)
